d00f4650f894e8fcb76af3531c913a25f50b103b
Marcella1910 <marcellagaurink@gmail.com>
2024-09-26 13:03:11 -0300

    exercicio 1

---
+++ exercicio1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exercicio 1</title>
+</head>
+
+<body>
+  <?php
+    echo "<img src = 'https://picsum.photos/500/600'><br>";
+    echo "<h1>Imagem da semana 2</h1>";
+
+    echo "<h1>URLs Absolutas</h1>";
+    echo "<a href = 'https://www.amazon.com.br/ref=nav_logo'>Amazon</a><br><br>";
+    echo "<a href = 'https://www.youtube.com/'>YouTube</a>";
+
+    echo "<h1>URLs Relativas</h1>";
+    echo "<a href = 'paginaTartaglia.html'>Tartaglia</a><br><br>";
+    echo "<a href = 'paginaAventurine.html'>Aventurine</a>";
+?>
+
+
+
+</body>
+</html>
